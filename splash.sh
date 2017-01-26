3c3a38899934b1423eabe96c4b4d117a5534c92c
Saravanan Veerayah Jayaraman <vjsara70@gmail.com>
2017-01-27 05:14:48 +0600

    Update splash.sh

--- splash.sh
+++ splash.sh
@@ -1,2 +1,2 @@
 sudo sed -ie 's/console=tty1/console=tty2/g' /boot/cmdline.txt
-sudo echo ' logo.nologo loglevel=3' >> /boot/cmdline.txt
+sudo sed -ie 's/rootwait/rootwait logo.nologo loglevel=3/g' /boot/cmdline.txt
