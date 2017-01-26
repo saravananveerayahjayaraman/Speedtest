1357f5addf69598feb16255d48b3c9c66f69a0ab
Saravanan Veerayah Jayaraman <vjsara70@gmail.com>
2017-01-27 05:27:38 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 echo Installing Dewhirst Splash For Appliance
 echo ========================================
 wget https://raw.githubusercontent.com/vjsaravanan/Speedtest/master/splash.mov
-wget https://raw.githubusercontent.com/vjsaravanan/Speedtest/master/asplashscreen
-cp asplashscreen /etc/init.d/asplashscreen
-chmod a+x /etc/asplashscreen
+wget https://raw.githubusercontent.com/vjsaravanan/Speedtest/master/splash.png
+wget https://raw.githubusercontent.com/vjsaravanan/Speedtest/master/asplashscreen_mov
+wget https://raw.githubusercontent.com/vjsaravanan/Speedtest/master/asplashscreen_png
+cp asplashscreen_mov /etc/init.d/asplashscreen
+chmod a+x /etc/init.d/asplashscreen
+insserv /etc/init.d/asplashscreen 
